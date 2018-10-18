3e0ff862512d5c2d224c4ea8e67a7d9ca90c5ff7
RedPlayer autotests <andrii.lukianets@leiainc.com>
2018-10-18 16:59:00 +0300

    up

--- tests/test_main.py
+++ tests/test_main.py
@@ -13,48 +13,48 @@ class Test_Main_Page(driver, Actions):
     Post-conditions:
     """
 
-    # def test_slider_page_scroll(self, driver):
-    #
-    #     # Slider testing is works
-    #     time.sleep(5)
-    #     assert Actions.isObjectExistsID(self, driver, MainLocators.LOGO_ID) is True
-    #
-    #     # Test swipe to right
-    #     slider_app_name_before_swipe = Actions.getTextID(self, driver, MainLocators.SLIDER_APP_NAME_ID)
-    #     Actions.swipeSliderToRight(self, driver)
-    #     slider_app_name_after_swipe = Actions.getTextID(self, driver, MainLocators.SLIDER_APP_NAME_ID)
-    #
-    #     assert slider_app_name_before_swipe != slider_app_name_after_swipe
-    #
-    #     # Test swipe to left
-    #     Actions.swipeSliderToLeft(self, driver)
-    #     Actions.swipeSliderToLeft(self, driver)
-    #     Actions.swipeSliderToLeft(self, driver)
-    #
-    #     slider_app_name_after_after_swipe = Actions.getTextID(self, driver, MainLocators.SLIDER_APP_NAME_ID)
-    #
-    #     assert slider_app_name_after_after_swipe != slider_app_name_after_swipe
-    #     assert  slider_app_name_after_after_swipe != slider_app_name_before_swipe
-    #
-    #     # Test open app page
-    #     assert Actions.isObjectExistsID(self, driver, AppPageLocators.GALLERY_BLOCK_ID) is False
-    #
-    #     Actions.clickButtonID(self, driver, MainLocators.SLIDER_WHOLE_APP_BLOCK_ID)
-    #
-    #     assert Actions.isObjectExistsID(self, driver, AppPageLocators.GALLERY_BLOCK_ID) is True
-    #
-    #     Actions.clickButtonID(self, driver, AppPageLocators.BACK_BUTTON_ID)
-    #
-    #     assert Actions.isObjectExistsID(self, driver, AppPageLocators.GALLERY_BLOCK_ID) is False
-    #
-    #     # Vertical scrolling testing
-    #     assert Actions.isObjectExistsID(self, driver, MainLocators.SEARCH_BUTTON_ID) is True
-    #     assert Actions.isObjectExistsID(self, driver, MainLocators.EDITOR_CHOICE_TITLE_ID) is False
-    #
-    #     Actions.scrollPageToUp(self, driver)
-    #
-    #     assert Actions.isObjectExistsID(self, driver, MainLocators.SEARCH_BUTTON_ID) is False
-    #     assert Actions.isObjectExistsID(self, driver, MainLocators.EDITOR_CHOICE_TITLE_ID) is True
+    def test_slider_page_scroll(self, driver):
+
+        # Slider testing is works
+        time.sleep(5)
+        assert Actions.isObjectExistsID(self, driver, MainLocators.LOGO_ID) is True
+
+        # Test swipe to right
+        slider_app_name_before_swipe = Actions.getTextID(self, driver, MainLocators.SLIDER_APP_NAME_ID)
+        Actions.swipeSliderToRight(self, driver)
+        slider_app_name_after_swipe = Actions.getTextID(self, driver, MainLocators.SLIDER_APP_NAME_ID)
+
+        assert slider_app_name_before_swipe != slider_app_name_after_swipe
+
+        # Test swipe to left
+        Actions.swipeSliderToLeft(self, driver)
+        Actions.swipeSliderToLeft(self, driver)
+        Actions.swipeSliderToLeft(self, driver)
+
+        slider_app_name_after_after_swipe = Actions.getTextID(self, driver, MainLocators.SLIDER_APP_NAME_ID)
+
+        assert slider_app_name_after_after_swipe != slider_app_name_after_swipe
+        assert  slider_app_name_after_after_swipe != slider_app_name_before_swipe
+
+        # Test open app page
+        assert Actions.isObjectExistsID(self, driver, AppPageLocators.GALLERY_BLOCK_ID) is False
+
+        Actions.clickButtonID(self, driver, MainLocators.SLIDER_WHOLE_APP_BLOCK_ID)
+
+        assert Actions.isObjectExistsID(self, driver, AppPageLocators.GALLERY_BLOCK_ID) is True
+
+        Actions.clickButtonID(self, driver, AppPageLocators.BACK_BUTTON_ID)
+
+        assert Actions.isObjectExistsID(self, driver, AppPageLocators.GALLERY_BLOCK_ID) is False
+
+        # Vertical scrolling testing
+        assert Actions.isObjectExistsID(self, driver, MainLocators.SEARCH_BUTTON_ID) is True
+        assert Actions.isObjectExistsID(self, driver, MainLocators.EDITOR_CHOICE_TITLE_ID) is False
+
+        Actions.scrollPageToUp(self, driver)
+
+        assert Actions.isObjectExistsID(self, driver, MainLocators.SEARCH_BUTTON_ID) is False
+        assert Actions.isObjectExistsID(self, driver, MainLocators.EDITOR_CHOICE_TITLE_ID) is True
 
     def test_top_picks_editors_choice_blocks(self, driver):
         # Top picks block swipe
